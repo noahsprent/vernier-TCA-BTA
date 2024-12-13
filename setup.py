cd9e81c189669d04128b9f0d59ff8e420d063bc6
Noah Sprent <noahsprent@gmail.com>
2024-12-13 19:38:07 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="vernier-FPH-BTA",
-    version="0.0.4",
+    version="0.1.0",
     license_files = ('LICENSE.txt',),
     description="The plugin allows you to read pH into the pioreactor app using the Vernier FPH-BTA sensor.",
     long_description=open("README.md").read(),
